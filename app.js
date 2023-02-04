a6e010db01b17ca62f2a026410f83aa4bce67445
Kakuli <kakulibasu82@gmail.com>
2023-02-04 22:43:50 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+const https = require("https");
+const { response } = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    // console.log(firstName, lastName, email);
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const JSONData = JSON.stringify(data);
+
+    const url = "https://us11.api.mailchimp.com/3.0/lists/dc326de8f1";
+
+    const options = {
+        method: "POST",
+        auth: "kakuli:83cb87e4cc2d3b9630e972ec2dd7e94e-us11"
+    };
+
+    const request = https.request(url, options, (response) => {
+        response.on("data", (data) => {
+            // console.log(JSON.parse(data));
+            console.log(response.statusCode);
+            if (response.statusCode === 200) {
+                res.sendFile(__dirname + "/success.html");
+            } else {
+                res.sendFile(__dirname + "/failure.html");
+            };
+        });
+    });
+
+    request.write(JSONData);
+    request.end();
+});
+
+app.post("/failure", (req, res) => {
+    res.redirect("/");
+});
+
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000.`);
+});
+
+// Mailchimp API:
+// API Key -
+// 83cb87e4cc2d3b9630e972ec2dd7e94e-us11
+
+// Audience ID -
+// dc326de8f1
