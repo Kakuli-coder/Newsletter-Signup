832584880125aa767e8bb92fd5ec45a1347e9c43
Kakuli <kakulibasu82@gmail.com>
2023-03-23 16:40:50 +0530

    app update

--- app.js
+++ app.js
@@ -44,8 +44,7 @@ app.post("/", (req, res) => {
     const request = https.request(url, options, (response) => {
         response.on("data", (data) => {
             // console.log(JSON.parse(data));
-            console.log(response.statusCode);
-            if (response.statusCode === 200) {
+            if (response.statusCode === 200) {              
                 res.sendFile(__dirname + "/success.html");
             } else {
                 res.sendFile(__dirname + "/failure.html");
